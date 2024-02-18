89f3190322fff1e31bc6b25ba585f1fafd286cdc
Dharshiramu <159539611+Dharshiramu@users.noreply.github.com>
2024-02-18 10:42:22 +0530

    Successfully completed task 1

---
+++ task1.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int minRange = 1;
+        int maxRange = 100;
+        int attemptsLimit = 5;
+        int score = 0;
+
+        System.out.println("Welcome to the Guess the Number game!");
+
+        boolean playAgain = true;
+        while (playAgain) {
+            int randomNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            System.out.println("I've generated a number between " + minRange + " and " + maxRange + ". Try to guess it!");
+
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+            while (attempts < attemptsLimit && !guessedCorrectly) {
+                System.out.print("Enter your guess: ");
+                int guess = scanner.nextInt();
+                attempts++;
+
+                if (guess == randomNumber) {
+                    System.out.println("Congratulations! You've guessed the number correctly in " + attempts + " attempts.");
+                    guessedCorrectly = true;
+                    score++;
+                } else if (guess < randomNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("Sorry, you've run out of attempts. The correct number was: " + randomNumber);
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainResponse = scanner.next().toLowerCase();
+            if (!playAgainResponse.equals("yes")) {
+                playAgain = false;
+            }
+        }
+
+        System.out.println("Game over! Your score is: " + score);
+        scanner.close();
+    }
+}
